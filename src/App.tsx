d1986118e7372f4a3776f22a3756aec83ca5f486
Jordan Aston <jordanaston93@gmail.com>
2023-11-02 13:43:44 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import CryptoCard from "./components/CryptoCard";
+
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <div>
+        <CryptoCard />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/CryptoCard.tsx
@@ -0,0 +1,8 @@
+
+
+
+const CryptoCard: React.FC = (): JSX.Element => {
+  return <div>Hello</div>;
+};
+
+export default CryptoCard;
